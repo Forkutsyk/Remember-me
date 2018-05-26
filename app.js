9248a64da112ba9e7c211e4b844311c0c4a87378
Forkutsyk <oleksand.poplavskiy@gmail.com>
2018-05-26 16:34:11 +0300

    1 js file+ litle change in creat point
    now can add obj

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
-function AddLS() {
+window.onload = function() {
 
+var todoList = [];
+
+ document.getElementById('add').onclick = function add() {
+   var d = document.getElementById('in').value;
+  // {todo: addsmth, check: false}
+  var temp = {};
+   temp.todo = d;
+   temp.check = false;
+   var i = todoList.length;
+   todoList[i] = temp;
+   console.log(todoList);
+ }
+ function out() {
+   var out = '';
+   for (var key in todoList){
+     out += todoList[key].todo + '<br>';
+   }
+ }
 }
