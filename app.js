4fd0f826ee77fd7822f95bbe5f07b79f63225278
Forkutsyk <oleksand.poplavskiy@gmail.com>
2018-05-30 10:54:54 +0300

    2 changes
    1.buttonswas changed.
    2. list was changed.
    (tried to do with time in local storage)

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 window.onload = function() {
-
 var todoList = [];
-
  document.getElementById('add').onclick = function add() {
-   var d = document.getElementById('in').value;
-  // {todo: addsmth, check: false}
+   var d = document.getElementById('in','time').value;
+  // {todo: addsmth, time:00:00, check: false}
   var temp = {};
    temp.todo = d;
    temp.check = false;
+   temp.time = d;
    var i = todoList.length;
    todoList[i] = temp;
    console.log(todoList);
