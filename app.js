0fe3a86bf11f401591d129a4f01b379bbf795ade
Forkutsyk <oleksand.poplavskiy@gmail.com>
2018-06-06 10:14:54 +0300

    4 changes
    -add posibility creating task with date and time
    -change color of buttons
    - few small changes like gaps etc.

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-window.onload = function() {
+window.onload = function(){
 var todoList = [];
  document.getElementById('add').onclick = function add() {
-   var d = document.getElementById('in','time').value;
-  // {todo: addsmth, time:00:00, check: false}
-  var temp = {};
+   var d = document.getElementById('in').value;
+   var t = document.getElementById('time').value;
+  // {todo: addsmth, time: dd.mm.yyyyT00:00, check: false}
+   var temp = {};
    temp.todo = d;
    temp.check = false;
-   temp.time = d;
+   temp.time = t;
    var i = todoList.length;
    todoList[i] = temp;
    console.log(todoList);
-   localStorage.setItem('todo',JSON.stringify(todoList))
+   if (todoList === '') {
+     alert("You must write something!");
+   }
+   localStorage.setItem('todo',JSON.stringify(todoList));
  }
 }
+/*'todo',JSON.stringify(todoList)*/
--- list.js
+++ list.js
@@ -1,10 +1,10 @@
 window.onload = function() {
-  if (localStorage.getItem('todo')!= undefined) {
+  if (localStorage.getItem('todo') != undefined) {
     todoList = JSON.parse(localStorage.getItem('todo'));
     out();
   }
 function out(){
-  var out = '';
+  var out = ' ';
   for (var key in todoList){
     if (todoList[key].check == true){
       out += '<input type="checkbox" checked>';
@@ -12,11 +12,14 @@ function out(){
     else {
       out += '<input type="checkbox">';
     }
-    out += todoList[key].todo + '<br>';
+    out += todoList[key].todo +'<br>'+ todoList[key].time + '<br>';
   }
   document.getElementById('out').innerHTML = out;
   out();
 }
+
+
+/* Початкова таблиця*/
 var myNodelist = document.getElementsByTagName("LI");
 var i;
 for (i = 0; i < myNodelist.length; i++) {
